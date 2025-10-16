348113f6ea29ab4e172774e8d7e5933eae4b532d
MihailRis <mexaruc@gmail.com>
2025-10-16 21:26:05 +0300

    update Shadows.cpp

--- src/graphics/core/Shadows.cpp
+++ src/graphics/core/Shadows.cpp
@@ -1,10 +1,11 @@
-#include "Shadows.hpp"
-
-#include <GL/glew.h>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/norm.hpp>
 #include <glm/gtc/constants.hpp>
 #include "glm/gtc/matrix_transform.hpp"
+
+#include "Shadows.hpp"
+
+#include <GL/glew.h>
 
 #include "assets/Assets.hpp"
 #include "graphics/core/DrawContext.hpp"
