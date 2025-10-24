5b6c9e4f40b21053b1d21a4d9f51002d1ec4959c
eliotbyte <eliotbyte@proton.me>
2025-10-24 15:51:20 +0300

    add :block placement for single blocks

--- src/world/generator/StructurePlacement.hpp
+++ src/world/generator/StructurePlacement.hpp
@@ -27,12 +27,22 @@ struct LinePlacement {
     }
 };
 
+struct BlockPlacement {
+    blockid_t block;
+    glm::ivec3 position;
+    uint8_t rotation;
+
+    BlockPlacement(blockid_t block, glm::ivec3 position, uint8_t rotation)
+        : block(block), position(std::move(position)), rotation(rotation) {
+    }
+};
+
 struct Placement {
     int priority;
-    std::variant<StructurePlacement, LinePlacement> placement;
+    std::variant<StructurePlacement, LinePlacement, BlockPlacement> placement;
 
     Placement(
         int priority,
-        std::variant<StructurePlacement, LinePlacement> placement
+        std::variant<StructurePlacement, LinePlacement, BlockPlacement> placement
     ) : priority(priority), placement(std::move(placement)) {}
 };
